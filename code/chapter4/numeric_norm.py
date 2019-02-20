5efb7fda03033d39275c644aff7c50269071ebdb
Jan Marais <maraisjandre9@gmail.com>
2019-02-20 20:16:19 +0200

    not sure

--- code/chapter4/numeric_norm.py
+++ code/chapter4/numeric_norm.py
@@ -5,7 +5,7 @@ import numpy as np
 import seaborn as sns
 
 
-path = Path('../../data')
+path = Path('../data')
 
 df = pd.read_csv(path/'adult/adult.csv')
 
@@ -24,10 +24,10 @@ def power_norm(x):
 fig, ax = plt.subplots(1,3)
 
 sns.jointplot(x1, x2, alpha=0.3)
-plt.savefig('../../writing/figures/cont_vars.pdf', bbox_inches='tight')
+plt.savefig('../writing/figures/cont_vars.png', bbox_inches='tight')
 
 sns.jointplot(gaus_norm(x1), gaus_norm(x2), alpha=0.3)
-plt.savefig('../../writing/figures/gaus_norm.pdf', bbox_inches='tight')
+plt.savefig('../writing/figures/gaus_norm.png', bbox_inches='tight')
 
 sns.jointplot(power_norm(x1), power_norm(x2), alpha=0.3)
-plt.savefig('../../writing/figures/power_norm.pdf', bbox_inches='tight')
+plt.savefig('../writing/figures/power_norm.png', bbox_inches='tight')
