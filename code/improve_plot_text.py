2bc9e205be4f20e9903bd5884c8e21fa6e6a11f2
Jan Marais <maraisjandre9@gmail.com>
2019-02-25 23:47:36 +0200

    general

--- code/improve_plot_text.py
+++ code/improve_plot_text.py
@@ -4,7 +4,7 @@ from fastai_ext.plot_utils import plot_best, plot_over_epochs, display_embs
 from matplotlib import pyplot as plt
 
 path = Path('../data/adult')
-experiment_name, exp_path = create_experiment('residual_depth', path)
+experiment_name, exp_path = create_experiment('sgd_1cyc', path)
 
 config_df, recorder_df, param_names, metric_names = load_results(exp_path)
 summary_df = summarise_results(recorder_df, param_names, metric_names)
